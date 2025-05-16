749bf12a151691ddfaa1dabca1d55202160f155e
Daniel Caridade <20211588@novaims.unl.pt>
2025-05-16 22:56:38 +0100

    Update parser.py

--- code/utils/parser.py
+++ code/utils/parser.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from pathlib import Path
 
-def load_relationship_matrix(path='data/seating_data.xlsx'):
-    df = pd.read_excel(path, index_col=0)
-    return df.values
+def load_relationship_matrix():
+    data_path = Path(__file__).resolve().parent.parent.parent / 'data' / 'seating_data.xlsx'
+    df = pd.read_excel(data_path, index_col=0)
+    return df
