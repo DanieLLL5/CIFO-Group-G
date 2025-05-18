172dd4fe336bdc307815f6ff54e72a3d87de4f30
Daniel Caridade <20211588@novaims.unl.pt>
2025-05-18 21:48:52 +0100

    Create mutation.py

---
+++ code/optimizers/genetic_algorithms/mutation.py
@@ -0,0 +1,81 @@
+import random
+from utils.WeddingSeatingHelper import get_neighbours, sorter2
+
+def mutation(individual, swap=True, table_flip=True, relationship_augmenter=True):
+    """
+    Applies a random mutation to an individual based on enabled mutation types.
+    """
+    # Collect enabled mutation types
+    mutations = []
+    if swap:
+        mutations.append("swap")
+    if table_flip:
+        mutations.append("table_flip")
+    if relationship_augmenter:
+        mutations.append("relationship_augmenter")
+
+    if not mutations:
+        raise ValueError("Please select at least one mutation method.")
+
+    mutation = random.choice(mutations)
+
+    if mutation == "swap":
+        # Swap two guests from neighboring solution
+        individual = random.choice(get_neighbours(individual))
+
+    elif mutation == "table_flip":
+        individual_transformed = []
+
+        for i in range(len(individual)):
+            for j in range(len(individual[i])):
+                individual_transformed.append([individual[i][j], i])
+
+        individual_transformed.sort(key=sorter2)
+
+        for i in range(len(individual_transformed) // 2):
+            i1 = individual_transformed[i]
+            i2 = individual_transformed[-i - 1]
+            i1[1], i2[1] = i2[1], i1[1]
+
+        new_individual = [[] for _ in range(8)]
+        for guest_id, table_id in individual_transformed:
+            new_individual[table_id].append(guest_id)
+
+        individual = new_individual
+
+    elif mutation == "relationship_augmenter":
+        while True:
+            guest_table = random.randint(0, 7)
+            guest_seating = random.randint(0, 7)
+            guest = individual[guest_table][guest_seating]
+
+            related_guest = guest + 1 if guest != 64 else 63
+
+            related_guest_table = None
+            related_guest_seating = None
+
+            for i in range(8):
+                for j in range(8):
+                    if individual[i][j] == related_guest:
+                        related_guest_table = i
+                        related_guest_seating = j
+                        break
+                if related_guest_table is not None:
+                    break
+
+            if guest_table != related_guest_table:
+                break
+
+        swap_choices = [
+            i for i in range(8)
+            if i != related_guest_seating
+        ]
+        swapped_guest = individual[related_guest_table][random.choice(swap_choices)]
+
+        individual[guest_table].remove(guest)
+        individual[related_guest_table].remove(swapped_guest)
+
+        individual[related_guest_table].append(guest)
+        individual[guest_table].append(swapped_guest)
+
+    return individual
