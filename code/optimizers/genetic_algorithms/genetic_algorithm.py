0947a7d00520bedf1616580747c6927dcaea5b8b
Daniel Caridade <20211588@novaims.unl.pt>
2025-05-21 10:35:29 +0100

    Update genetic_algorithm.py

--- code/optimizers/genetic_algorithms/genetic_algorithm.py
+++ code/optimizers/genetic_algorithms/genetic_algorithm.py
@@ -4,12 +4,11 @@ import numpy as np
 class GeneticAlgorithm:
     def __init__(self, helper, pop_size=100, num_gen=50, p_xo=0.8, p_m=0.05, selection_method="battle",
                  elitism=False, n_elite=10, n_battles=5, p_pity=0.0001,
-                 swap=True, table_flip=True, relationship_augmenter=True,
-                 selection_func=None, crossover_func=None, mutation_func=None):
+                 swap=True, table_flip=True, relationship_augmenter=True):
         """
         Initialize the genetic algorithm.
 
-        :param helper: helper object/class with fitness and generate_solution methods
+        :param helper: Helper object/class with fitness, generate_solution, selection, crossover, and mutation methods
         :param pop_size: Population size
         :param num_gen: Number of generations
         :param p_xo: Probability of crossover
@@ -22,11 +21,7 @@ class GeneticAlgorithm:
         :param swap: Enable swap mutation
         :param table_flip: Enable table flip mutation
         :param relationship_augmenter: Enable relationship augmenter mutation
-        :param selection_func: Function to handle selection
-        :param crossover_func: Function to handle crossover
-        :param mutation_func: Function to handle mutation
         """
-
         self.helper = helper
         self.pop_size = pop_size
         self.num_gen = num_gen
@@ -41,20 +36,27 @@ class GeneticAlgorithm:
         self.table_flip = table_flip
         self.relationship_augmenter = relationship_augmenter
 
-        # Dependency injections or default implementations
-        self.selection_func = selection_func
-        self.crossover_func = crossover_func
-        self.mutation_func = mutation_func
-
     def run(self):
-        #Generate a random initial population
+        # Generate initial population
         population = [self.helper.generate_solution() for _ in range(self.pop_size)]
 
-        for i in range(self.num_gen):
-            print(f"gen {i}")
-            #Probablistically the best individuals pass the selection process
-            selected_population = self.selection_func(
-            population = self.selection_func(
+        for gen in range(self.num_gen):
+            print(f"Generation {gen}")
+
+            # Evaluate fitness
+            fitnesses = [self.helper.fitness(ind) for ind in population]
+            best_fitness = max(fitnesses)
+            avg_fitness = np.mean(fitnesses)
+            print(f"Best: {best_fitness}, Average: {avg_fitness}")
+
+            # Elitism: preserve best individuals
+            elites = []
+            if self.elitism:
+                elite_indices = np.argsort(fitnesses)[-self.n_elite:]
+                elites = [population[i] for i in elite_indices]
+
+            # Selection
+            selected_population = self.helper.selection(
                 population,
                 self.pop_size,
                 self.helper,
@@ -65,54 +67,41 @@ class GeneticAlgorithm:
                 p_pity=self.p_pity
             )
 
-            #Some will do crossover, some will mutate and others will stay as is for the next generation
-            for i in range(self.pop_size // 2):
-
+            # Generate new population through crossover and mutation
             new_population = []
-            crossedover_indexes = []
-            mutated_indexes = []
 
-            #Crossover
-            for i in range(0,self.pop_size-1,2):
+            for i in range(0, self.pop_size - 1, 2):
+                parent1, parent2 = selected_population[i], selected_population[i + 1]
+
                 if random.random() < self.p_xo:
-                    selected_population[i], selected_population[-i-1] = self.crossover_func(
-                        selected_population[i], selected_population[-i-1])
-                elif random.random() < self.p_m * 2:
-                    if random.random() < 0.5:
-                        selected_population[i] = self.mutation_func(
-                            selected_population[i], self.helper, self.swap, self.table_flip, self.relationship_augmenter)
-                    else:
-                        selected_population[-i-1] = self.mutation_func(
-                            selected_population[-i-1], self.helper, self.swap, self.table_flip, self.relationship_augmenter)
-                    child1, child2 = self.crossover_func(
-                        population[i], population[i+1])
-                    
-                    crossedover_indexes.append(i)
-                    crossedover_indexes.append(i+1)
-                    new_population.append(child1)
-                    new_population.append(child2)
+                    child1, child2 = self.helper.crossover(parent1, parent2)
+                else:
+                    child1, child2 = parent1, parent2
 
-            #Mutation to individuals that didn't crossover
-            for i in range(self.pop_size):
-                if i not in crossedover_indexes and random.random() < self.p_m:
-                    mutated = self.mutation_func(
-                        population[i], self.helper, self.swap, self.table_flip, self.relationship_augmenter)
-                    
-                    mutated_indexes.append(i)
-                    new_population.append(mutated)
+                if random.random() < self.p_m:
+                    child1 = self.helper.mutation(child1, self.helper, self.swap, self.table_flip, self.relationship_augmenter)
+                if random.random() < self.p_m:
+                    child2 = self.helper.mutation(child2, self.helper, self.swap, self.table_flip, self.relationship_augmenter)
 
-            #Adding the individuals that didn't crossover or mutate
-            for i in range(self.pop_size):
-                if i not in crossedover_indexes and i not in mutated_indexes:
-                    new_population.append(population[i])
+                new_population.extend([child1, child2])
 
-            population = new_population[:self.pop_size]
+            # If population size is odd, handle the last individual
+            if len(new_population) < self.pop_size:
+                leftover = selected_population[-1]
+                if random.random() < self.p_m:
+                    leftover = self.helper.mutation(leftover, self.helper, self.swap, self.table_flip, self.relationship_augmenter)
+                new_population.append(leftover)
 
-            #Print the best and average fitness of the population
-            print(max([self.helper.fitness(individual) for individual in population]),
-                  np.mean([self.helper.fitness(individual) for individual in population]))
+            # Add elites if elitism is enabled
+            if self.elitism:
+                new_population = elites + new_population
+                new_population = new_population[:self.pop_size]
 
-        best_fitness = max([self.helper.fitness(individual) for individual in population])
-        best_individual = population[np.argmax([self.helper.fitness(individual) for individual in population])]
+            population = new_population
+
+        # Final fitness evaluation
+        fitnesses = [self.helper.fitness(ind) for ind in population]
+        best_fitness = max(fitnesses)
+        best_individual = population[np.argmax(fitnesses)]
 
         return best_fitness, best_individual
